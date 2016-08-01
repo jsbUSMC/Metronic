e4fcf8eb6d4820d32069f685fba4fe4435831801
John Bissonette <jbissonette@UR.Rochester.edu>
2016-07-31 23:55:17 -0400

    added a bundle config for bundling the assets needed and caching them instead of working with serenity's built in trash

---
+++ Metronic/Metronic.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Optimization;
+
+namespace Metronic
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            StyleBundle metronic = new StyleBundle("~/Content/Stylez");
+            metronic.Include(
+                "~/Content/font-open-sans.css",
+                "~/Content/assets/global/plugins/font-awesome/css/font-awesome.css",
+                "~/Content/assets/global/plugins/simple-line-icons/simple-line-icons.css",
+                "~/Content/assets/global/plugins/bootstrap/css/bootstrap.css",
+                "~/Content/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.css",
+                "~/Content/assets/global/css/components-md.css",
+                "~/Content/assets/global/css/plugins-md.css",
+                "~/Content/assets/layouts/layout/css/layout.css",
+                "~/Content/assets/layouts/layout/css/custom.css"
+                );
+        }
+    }
+}
